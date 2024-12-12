875b719a357dbef619a83ad718e19c50b250347e
mishkao <160684168+mishkao@users.noreply.github.com>
2024-12-12 10:27:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu')
+    menu.classList.toggle('hidden');
+}
+function toggleNumb(){
+    const num = document.getElementById('numb')
+    numb.classList.toggle('hidden')
+}
